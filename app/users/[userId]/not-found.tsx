190d93df08046fdd36151005b305ac9d580e69bd
Maria <sawaby@users.noreply.github.com>
2023-11-04 09:23:53 -0700

    practice server actions

---
+++ app/users/[userId]/not-found.tsx
@@ -0,0 +1,6 @@
+
+export default function NotFound() {
+  return (
+    <h1>The Requested User does not Exist.</h1>
+  )
+}
---
+++ lib/getUserPost.ts
@@ -0,0 +1,10 @@
+
+
+export default async function getUser(userId: string) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, { next: { revalidate: 60 }})
+
+    if (!res.ok) return ''
+
+    return res.json()
+
+}
